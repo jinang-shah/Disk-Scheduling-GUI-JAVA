6d720fc2a2d37fbd8b8f8cf20f0863bfe9fc03fe
jinang-shah <sjinang@gmail.com>
2020-10-21 00:06:19 +0530

    c scan complet

--- src/NewJFrame1.java
+++ src/NewJFrame1.java
@@ -151,6 +151,94 @@ static int[] scan(int arr[], int head, String direction)
     
     return a;
 }
+
+static int[] cscan(int arr[], int head) 
+{ 
+    int size=arr.length;
+    int seek_count = 0; 
+    int distance, cur_track; 
+    Vector<Integer> left = new Vector<Integer>(),
+                    right = new Vector<Integer>(); 
+    Vector<Integer> seek_sequence = new Vector<Integer>(); 
+    Vector<Integer> list = new Vector<Integer>();
+    int [] a = new int[size+3];
+    list.add(head);
+    
+    // appending end values 
+    // which has to be visited 
+    // before reversing the direction 
+    left.add(0); 
+    right.add(199); 
+  
+    // tracks on the left of the 
+    // head will be serviced when 
+    // once the head comes back 
+    // to the beggining (left end). 
+    for (int i = 0; i < size; i++) { 
+        if (arr[i] < head) 
+            left.add(arr[i]); 
+        if (arr[i] > head) 
+            right.add(arr[i]); 
+    } 
+  
+    // sorting left and right vectors 
+    Collections.sort(left);
+    Collections.sort(right); 
+  
+    // first service the requests 
+    // on the right side of the 
+    // head. 
+    for (int i = 0; i < right.size(); i++) { 
+        cur_track = right.get(i); 
+        // appending current track to seek sequence 
+        seek_sequence.add(cur_track); 
+  
+        // calculate absolute distance 
+        distance = Math.abs(cur_track - head); 
+  
+        // increase the total count 
+        seek_count += distance; 
+  
+        // accessed track is now new head 
+        head = cur_track; 
+    } 
+  
+    // once reached the right end 
+    // jump to the beggining. 
+    head = 0; 
+  
+    // Now service the requests again 
+    // which are left. 
+    for (int i = 0; i < left.size(); i++) { 
+        cur_track = left.get(i); 
+  
+        // appending current track to seek sequence 
+        seek_sequence.add(cur_track); 
+  
+        // calculate absolute distance 
+        distance = Math.abs(cur_track - head); 
+  
+        // increase the total count 
+        seek_count += distance; 
+  
+        // accessed track is now the new head 
+        head = cur_track; 
+    } 
+ 
+    for (int i = 0; i < seek_sequence.size(); i++)
+    {
+        list.add(seek_sequence.get(i));
+        
+    }
+    
+    for (int i = 0; i < list.size(); i++)
+    {
+        a[i]=list.get(i);
+        System.out.println(a[i]);
+    }
+    
+    return a; 
+} 
     
     
 
@@ -214,9 +302,7 @@ static int[] scan(int arr[], int head, String direction)
         jLabel5.setForeground(new java.awt.Color(255, 255, 255));
         jLabel5.setText("R/W Head Position :");
         getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 210, -1, -1));
-
-        jTextField2.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
-        getContentPane().add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 207, 60, 20));
+        getContentPane().add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 210, 60, 30));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -271,26 +357,28 @@ static int[] scan(int arr[], int head, String direction)
         else if(algo=="SCAN"){
             int [] a = new int[strArray.length];
             int [] c = new int[strArray.length+2];
-            int [] b = new int[strArray.length+2];
 
             for(int i=0;i<strArray.length;i++){
                a[i]=Integer.parseInt(strArray[i]);
             }
-            
-       /*     for(int i=0;i<list.size();i++){
-                a[i]=list.get(i);
-            }*/
 
-
-            b=a;
             c=scan(a,head,"left");
             new NewJFrame2(c,c).setVisible(true);
             this.setVisible(false);
         }
         
         
-        else{
-            
+        else if(algo=="C SCAN"){
+            int [] a = new int[strArray.length];
+            int [] c = new int[strArray.length+3];
+
+            for(int i=0;i<strArray.length;i++){
+               a[i]=Integer.parseInt(strArray[i]);
+            }
+
+            c=cscan(a,head);
+            new NewJFrame2(c,c).setVisible(true);
+            this.setVisible(false);
         }
              
         
